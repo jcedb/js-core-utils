a562d8e659c8a6f109310ee874882ed2c811b671
Dexter Louie Aniez <lodianiez@gmail.com>
2022-02-22 09:44:13 +0800

    added string and number utils with separate interface files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-const FIRST = "first shit.";
+import { getCurrencySymbol } from "./numbers.utils";
+import { mask }  from "./strings.utils"
+
+/** Modular export para mas lighweight ang library */
+export const currencySymbol = getCurrencySymbol
+export const maskString = mask
