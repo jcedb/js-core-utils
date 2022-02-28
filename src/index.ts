682df6c87760ed44642aeeb9837e6d74ead3fb2a
Ced <jcedb07@gmail.com>
2022-02-28 22:30:53 +0800

    removed isPlaineObject then applied it to isObject

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import * as objects from './modules/objects.module';
 import * as arrays from './modules/arrays.module';
 import * as functions from './modules/function.module';
 
-export const { hasValue, isEmpty, isArray, isObject, isType, isPlainObject } =
-  boolean;
+export const { hasValue, isEmpty, isArray, isObject, isType } = boolean;
 export const { fullname, mask, uppercase, ellipses } = strings;
 export const { getCurrencySymbol } = number;
 export const { objectLoop, immutate } = objects;
